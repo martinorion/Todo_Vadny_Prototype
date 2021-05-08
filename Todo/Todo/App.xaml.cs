a2abca4e9d68f69333b335cd6ba6868e75f3a14c
MartinHolena <martinkiko18@gmail.com>
2021-05-08 11:46:08 +0200

    Add project files.

---
+++ Todo/Todo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Todo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new TodoListPage())
+            {
+                BarTextColor = Color.White,
+                BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"]
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Todo/Todo/Views/TodoItemPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Todo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TodoItemPage : ContentPage
+    {
+        public TodoItemPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            TodoItemDatabase database = await TodoItemDatabase.Instance;
+            await database.SaveItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            TodoItemDatabase database = await TodoItemDatabase.Instance;
+            await database.DeleteItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void OnCancelClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
